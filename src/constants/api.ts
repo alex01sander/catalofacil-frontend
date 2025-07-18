bf27504408f206654c2a4dffefbfe51ad1a478ef
Alex Britto <alexsander01@hotmail.com.br>
2025-07-18 00:09:25 -0300

    api rodando

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:3000";
+export const API_URL = "https://catalofacil-backend.onrender.com";
 import axios from "axios";
 
 axios.get(`${API_URL}/produtos`).then(res => console.log(res.data));
